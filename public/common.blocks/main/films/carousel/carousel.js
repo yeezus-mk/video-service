5904f81e1a645d24f36250056e235452ece9a958
= <=>
2021-03-12 00:18:40 +0400

    add films and slider for them

---
+++ public/common.blocks/main/films/carousel/carousel.js
@@ -0,0 +1,12 @@
+$('.carousel').slick({
+  infinite: true,
+  dots: true,
+  slidesToShow: 4,
+  slidesToScroll: 1,
+  variableWidth: true,
+  autoplay: true,
+  autoplaySpeed: 3500,
+  useTransforms: false,
+  arrows: true,
+  useCSS: false,
+});
