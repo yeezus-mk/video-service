df60574e571bb71e7854d4edc848c1526c1ddda8
= <=>
2021-03-13 00:50:55 +0400

    fix switchTabs for IE

--- public/common.blocks/tabs/switchTabs.js
+++ public/common.blocks/tabs/switchTabs.js
@@ -1,19 +1,19 @@
+const tabFilms = document.querySelector('#tab__films');
+const tabChannels = document.querySelector('#tab__channels');
+
 const tabs = document.querySelectorAll('.btn--tab');
 
 const films = document.querySelector('.films');
 const channels = document.querySelector('.channels');
 
-function switchTabs() {
-  tabs.forEach(tab => {
-    tab.addEventListener('change', (event) => {
-      if (event.target.value === 'channels') {
-        films.classList.add('hide');
-        channels.classList.remove('hide');
-      } else {
-        films.classList.remove('hide');
-        channels.classList.add('hide');
-      }
-    })
+document.addEventListener('DOMContentLoaded', function() {
+  tabFilms.addEventListener('click', function() {
+    films.classList.remove('hide');
+    channels.classList.add('hide');
   });
-};
+  tabChannels.addEventListener('click', function() {
+    films.classList.add('hide');
+    channels.classList.remove('hide');
+  });
+});
 
