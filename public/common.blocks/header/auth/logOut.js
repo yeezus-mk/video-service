b22669311247a2d919f58146bab009dca247c2eb
= <=>
2021-03-18 21:37:36 +0400

    fix main branch

--- public/common.blocks/header/auth/logOut.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-  
-  logOut.addEventListener('click', () => {
-    sessionStorage.setItem("is_reloaded", false);
-    window.location.reload();
-  })
-})
